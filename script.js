30b124f626cf9dbc117406c4a17247c137874017
EgorSad06 <143409348+EgorSad06@users.noreply.github.com>
2025-09-13 22:33:12 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -556,7 +556,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         shareText += `Присоединяйтесь и исследуйте!\n`; // Removed the URL from here
         shareText += `Наш телеграм канал:\nhttps://t.me/BeenInRussia\n`;
-        shareText += `Отметить свои достижения:`; // Removed the URL from here
+        shareText += `Отметить свои достижения:\nhttp://beeninrussia.ru/`; // Removed the URL from here
 
         const mapImage = await generateMapImage(); // Генерируем изображение карты
 
